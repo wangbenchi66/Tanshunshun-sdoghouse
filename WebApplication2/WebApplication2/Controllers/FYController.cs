b0bfd22fcb21770e00cd15057e61ce3baad0e4a0
wang183175 <18317529530@163.com>
2019-12-10 09:40:29 +0800

    修改分页功能的bug

--- WebApplication2/WebApplication2/Controllers/FYController.cs
+++ WebApplication2/WebApplication2/Controllers/FYController.cs
@@ -12,6 +12,7 @@ namespace WebApplication2.Controllers
         {
             return View();
         }
+        
         public DeluxeJsonResult QueryPageData(SearchModel<BookInfo> search)
         {
             int total = 0;
@@ -19,6 +20,7 @@ namespace WebApplication2.Controllers
             var totalPages = total % search.PageSize == 0 ? total / search.PageSize : total / search.PageSize + 1;
             return new DeluxeJsonResult(new
             {
+                //修改这了
                 Data = result,
                 Total = total,
                 TotalPages = totalPages
