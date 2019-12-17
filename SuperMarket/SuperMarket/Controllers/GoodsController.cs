6e826e1eae6c441a2a0a5ab8e8d58b8b8d407bbb
wangbenchi66 <69945864@qq.com>
2019-12-17 16:47:22 +0800

    查询

---
+++ SuperMarket/SuperMarket/Controllers/GoodsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using SuperMarketDal;
+
+namespace SuperMarket.Controllers
+{
+    public class GoodsController : Controller
+    {
+        // GET: Goods
+        public ActionResult GoodsIndex()
+        {
+            return View();
+        }
+        //public JsonResult select()
+        //{
+        //    var ef = {(from Goods in GoodsType
+        //             join tbMC in myModels.CY_FangTaiMC on tbfangtai.FangTaiMCID equals tbMC.FangTaiMCID
+        //             select new Fangtai{ }).ToList();
+        //}
+        [HttpPost]
+        public JsonResult select()
+        {
+            List<ModelCount> list = GoodsDAL.select();
+            JsonResult json = new JsonResult();
+            //var cc = from ss in list
+            //         select new
+            //         {
+            //             GoodsTypeName = ss.GoodsType.GoodsTypeName
+            //         };
+            JsonSerializerSettings setting = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.None
+            };
+            string ret = JsonConvert.SerializeObject(new {Data= list }, setting);
+            json.Data = new { Data = ret };
+            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return json;
+        }
+    }
+}
---
+++ SuperMarket/SuperMarketDal/GoodsDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketDal
+{
+   public  class GoodsDAL
+    {
+        public static List<ModelCount> select()
+        {
+            SuperMarketDB db = new SuperMarketDB();
+            List<ModelCount> list = (from cc in db.Goods
+                                     join aa in db.GoodsType
+                                     on cc.GoodsTypeId equals aa.GoodsTypeId
+                                     select new ModelCount
+                                     {
+                                         Goods = cc,
+                                         GoodsType = aa,
+                                     }).ToList();
+            return list;
+        }
+    }
+}
