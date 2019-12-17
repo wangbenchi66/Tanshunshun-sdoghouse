65f3eb17698f1e96a2530f846193ee35d71d32dc
wangbenchi66 <69945864@qq.com>
2019-12-17 19:48:44 +0800

    图表提交

---
+++ SuperMarket/SuperMarket/Controllers/EchartController.cs
@@ -0,0 +1,45 @@
+﻿using SuperMarketDal;
+using SuperMarketDal.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace SuperMarket.Controllers
+{
+    public class EchartController : Controller
+    {
+        // GET: Echart
+        //[AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[AllowAnonymous]
+        /// <summary>
+        /// 图表各类型统计
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult EcSellAnalyze()
+        {
+            List<EchartModel> list = EchartDal.EcSellAnalyze();
+            //var ss = JsonConvert.SerializeObject(list);
+            return Json(list);
+        }
+
+        /// <summary>
+        /// 图表统计
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult EcSellTOP()
+        {
+            List<EchartModel> list = EchartDal.EcSellTOP();
+            //var ss = JsonConvert.SerializeObject(list);
+            return Json(list);
+        }
+    }
+}
