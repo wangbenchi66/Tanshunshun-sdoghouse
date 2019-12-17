5954b791297853d4c38f4bb989ec49456772523c
wang183175 <18317529530@163.com>
2019-12-17 09:42:01 +0800

    匿名++++

---
+++ SuperMarket/SuperMarket/Filter/DeluxeUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SuperMarketDal;
+
+namespace SuperMarket.Filter
+{
+    public class DeluxeUser
+    {
+        public static employee CurrentMember { get; set; }
+    }
+}
