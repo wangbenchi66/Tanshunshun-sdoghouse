27d893925f676d481fe064cce9c523563cbdb882
wang183175 <18317529530@163.com>
2019-12-17 14:24:31 +0800

    登录匿名完善

--- SuperMarket/SuperMarket/Global.asax.cs
+++ SuperMarket/SuperMarket/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebApp.Portal.App_Start;
 
 namespace SuperMarket
 {
@@ -12,7 +13,7 @@ namespace SuperMarket
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
