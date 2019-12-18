da7229c0bcb2e1ffe6d81d68f7ddc9f64bea25c8
wang183175 <18317529530@163.com>
2019-12-18 13:12:48 +0800

    商品销售查询列表

---
+++ SuperMarket/SuperMarket/Controllers/SellController.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using SuperMarketDal;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SuperMarket.Controllers
+{
+    public class SellController : Controller
+    {
+        // GET: Sell
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SellList()
+        {
+            JsonResult json = new JsonResult();
+            sellDal dal = new sellDal();
+            List<sell> list = dal.List();
+            //var cc = from ss in list
+            //         select new
+            //         {
+            //             GoodsTypeName = ss.GoodsType.GoodsTypeName
+            //         };
+            //JsonSerializerSettings setting = new JsonSerializerSettings()
+            //{
+            //    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            //    Formatting = Formatting.None
+            //};
+            json.Data = new { Data = list };
+            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return json;
+        }
+    }
+}
---
+++ SuperMarket/SuperMarketDal/sellDal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperMarketDal
+{
+    public class sellDal
+    {
+        SuperMarketDB db = new SuperMarketDB();
+        /// <summary>
+        /// 商品销售列表
+        /// </summary>
+        /// <returns></returns>
+        public List<sell> List()
+        {
+            string sql = "select * from sell";
+            List<sell> list = db.Database.SqlQuery<sell>(sql).ToList();
+            return list;
+        }
+        /// <summary>
+        /// 添加商品销售信息
+        /// </summary>
+        /// <param name="sell"></param>
+        /// <returns></returns>
+        public int Insert(sell sell)
+        {
+            db.sell.Add(sell);
+            return db.SaveChanges();
+        }
+    }
+}
