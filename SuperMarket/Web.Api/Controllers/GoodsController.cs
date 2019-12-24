fa55184ac0afac8827b30730c65697075a640cd6
wangbenchi66 <69945864@qq.com>
2019-12-24 16:06:34 +0800

    添加引用

---
+++ SuperMarket/Web.Api/Controllers/GoodsController.cs
@@ -0,0 +1,20 @@
+﻿using SuperMarketDal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web.Api.Controllers
+{
+    public class GoodsController : ApiController
+    {
+        [HttpGet]
+        public object Get(string name = "")
+        {
+            return  GoodsDAL.select(name);
+            
+        }
+    }
+}
