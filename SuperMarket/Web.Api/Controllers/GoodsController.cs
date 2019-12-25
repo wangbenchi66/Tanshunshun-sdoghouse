a1e0913032a45b987d8ea0bcbea511e5d5b08432
wangbenchi66 <69945864@qq.com>
2019-12-25 16:59:05 +0800

    Web.Api Net包上传

--- SuperMarket/Web.Api/Controllers/GoodsController.cs
+++ SuperMarket/Web.Api/Controllers/GoodsController.cs
@@ -10,11 +10,56 @@ namespace Web.Api.Controllers
 {
     public class GoodsController : ApiController
     {
+        /// <summary>
+        /// 图表各类型统计
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public object Get(string name = "")
+        public object EcSellAnalyze()
         {
-            return  GoodsDAL.select(name);
+            return EchartDal.EcSellAnalyze();
             
         }
+
+        /// <summary>
+        /// 图表统计
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public object EcSellTOP()
+        {
+            return EchartDal.EcSellTOP();
+
+        }
+
+        /// <summary>
+        /// 图表列表统计
+        /// </summary>
+        /// <returns></returns>
+        public object EcSellList()
+        {
+            return EchartDal.EcSellList();
+
+        }
+        /// <summary>
+        /// 商品类别条件查询
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public object GoodsList(string name)
+        {
+            return EchartDal.GoodsList(name);
+        }
+
+        /// <summary>
+        /// 商品类别条件查询
+        /// </summary>
+        /// <returns></returns>
+        //[HttpPost]
+        //public object GoodsList2([FromBody]string from)
+        //{
+        //    ProductDetail
+        //    return EchartDal.GoodsList(name);
+        //}
     }
 }
